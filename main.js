b3ccd36f98bd4dba8dcf533e53a1cb094d86a7bf
Ludovic Cravoisier <scrapingrat@protonmail.com>
2020-11-05 13:32:58 +0100

    names

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-var http = require("http");
+let http = require('http');
+let fs = require('fs');
+let url = require('url')
 
-http.createServer(function (request, response)
+let server = http.createServer()
+server.on('request', (request, response) =>
 {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
-}).listen(8081);
-console.log('Server running at http://127.0.0.1:8081');
+	console.log(request.url)
+	let query = url.parse(request.url, true).query
+	if (query.name != undefined)
+	{
+	response.end("Hello " + query.name)
+	}
+	else
+	{
+		fs.readFile('index.html', (err, data) => 
+		{
+			if (err)
+			{
+				response.writeHead(404)
+				response.end("File not found\n")
+			}
+			else
+			{
+				response.writeHead(200, 
+				{
+					'Content-Type': 'text/plain; charset=utf-8'
+				})
+				response.end('Hello World\n')
+			}	
+		})
+	}
+})
+server.listen(8081)
 
+console.log('Server running at http://127.0.0.1:8081')
