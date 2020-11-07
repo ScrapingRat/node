506faf358777c48466b7c256deb4af02b5f2399a
Ludovic Cravoisier <scrapingrat@protonmail.com>
2020-11-07 19:03:14 +0100

    fs

--- go-fullstack/backend/controllers/stuff.js
+++ go-fullstack/backend/controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
 	const thingObject = JSON.parse(req.body.thing);
@@ -20,15 +21,27 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
+	const thingObject = req.file ?
+	{
+		...JSON.parse(req.body.thing),
+		imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+	} : { ...req.body };
 	Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
 		.then(things => res.status(200).json({ message: 'Item edited' }))
 		.catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing =  (req, res, next) => {
-	Thing.deleteOne({ _id: req.params.id })
-		.then(things => res.status(200).json({ message: 'Item deleted' }))
-		.catch(error => res.status(400).json({ error }));
+	Thing.findOne({ _id: req.params.id })
+	.then(thing => {
+		const filename = thing.imageUrl.split('/images/')[1];
+		fs.unlink(`images/${filename}`, () => {
+			Thing.deleteOne({ _id: req.params.id })
+			.then(things => res.status(200).json({ message: 'Item deleted' }))
+			.catch(error => res.status(400).json({ error }));
+		});
+	})
+	.catch(error => res.status(500).json({ error }));
 };
 
 exports.getAllStuff = (req, res, next) => {
