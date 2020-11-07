5ace479bd6e9ac681d7d2ea0957e928f60296d3e
Ludovic Cravoisier <scrapingrat@protonmail.com>
2020-11-07 16:26:41 +0100

    multer fix

--- go-fullstack/backend/app.js
+++ go-fullstack/backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://commerce:commerce1@cluster0.8kcer.mongodb.net/test?retryWrites=true&w=majority',
 	{	useNewUrlParser: true,
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
